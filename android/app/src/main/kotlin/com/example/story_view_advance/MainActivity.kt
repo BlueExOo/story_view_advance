a14e1d27ce312cd09be08823ad462ab82d993d7d
gauhun <gsrsocial99@gmail.com>
2024-09-07 00:04:06 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/story_view_advance/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.story_view_advance
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/story_view_advance.dart
@@ -0,0 +1,5 @@
+export 'widgets/story_image.dart';
+export 'widgets/story_video.dart';
+export 'widgets/story_view_advance.dart';
+export 'controller/story_controller.dart';
+export 'utils.dart';
